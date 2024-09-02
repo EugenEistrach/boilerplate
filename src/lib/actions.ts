974666ad781d2940449c8f7cfafea9d0ffc17168
EugenEistrach <eugen.eistrach@gmail.com>
2024-09-02 20:15:05 +0200

    improved error details

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,15 +4,29 @@ import { createSafeActionClient } from "next-safe-action"
 export const actionClient = createSafeActionClient({
   handleServerErrorLog: (error, utils) => {
     const { clientInput, bindArgsClientInputs, metadata, ctx } = utils
+    // Create a new scope for this error
+    Sentry.withScope(scope => {
+      // Set context data
+      // @ts-ignore
+      scope.setContext("clientInput", Object.fromEntries(clientInput))
 
-    // @ts-ignore
-    Sentry.setContext("clientInput", clientInput)
-    Sentry.setContext("bindArgsClientInputs", bindArgsClientInputs)
-    // @ts-ignore
-    Sentry.setContext("metadata", metadata)
-    Sentry.setContext("ctx", ctx)
+      scope.setContext(
+        "bindArgsClientInputs",
+        // @ts-ignore
+        Object.fromEntries(bindArgsClientInputs)
+      )
+      // @ts-ignore
+      scope.setContext("metadata", metadata)
+      // @ts-ignore
+      scope.setContext("ctx", ctx)
 
-    Sentry.captureException(error)
-    console.log("Server Error", error)
+      // Add extra data if needed
+      scope.setExtra("errorLocation", "serverAction")
+
+      // Capture the exception with the new scope
+      Sentry.captureException(error)
+    })
+
+    console.error("Server Error", error)
   }
 })
