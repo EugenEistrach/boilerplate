a182f43e36cecc893dcd71da7620c97cbb8009e3
EugenEistrach <eugen.eistrach@gmail.com>
2024-09-02 17:08:14 +0200

    fix

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,6 +2,7 @@ import { env } from "@/lib/env"
 import Database from "better-sqlite3"
 import { drizzle } from "drizzle-orm/better-sqlite3"
 
+import { migrate } from "drizzle-orm/better-sqlite3/migrator"
 import * as schema from "./schema"
 
 export const sqlite = new Database(env.DATABASE_URL)
@@ -12,3 +13,5 @@ sqlite.pragma("journal_mode = WAL")
 export const db = drizzle(sqlite, {
   schema: schema
 })
+
+migrate(db, { migrationsFolder: "src/db/migrations" })
